e561d3508b843b2129f042d34eb7abce56e899e4
Wilkenson <tcbwilkenson@gamilcom>
2023-11-27 13:10:24 -0500

    doc

--- cycleDectect.java
+++ cycleDectect.java
@@ -13,8 +13,9 @@ import java.util.HashSet;
 
 class Main {
     public static void main(String[] args) {
+        // directed graph > string key, list of string neighbors
         Map<String, List<String>> graph = new HashMap<>();
-
+        // add (k,v) pairs to graph
         graph.put("p2", List.of("p0", "p3"));
         graph.put("p1", List.of("p0", "p2"));
         graph.put("p3", List.of("p1"));
@@ -25,45 +26,45 @@ class Main {
 
     private static boolean isCyclic(Map<String, List<String>> graph) {
 
-        Set<String> visitedSet = new HashSet<>();
-        Stack<String> stack = new Stack<>();
+        Set<String> visitedSet = new HashSet<>(); // visited nodes
+        Stack<String> stack = new Stack<>(); // nodes in current path
 
         if (graph.isEmpty()) {
-            return false;
+            return false; // no cycle in empty graph
         }
 
-        for (String node : graph.keySet()) {
-            if (isCyclicUtil(graph, visitedSet, stack, node)) {
-                return true;
+        for (String node : graph.keySet()) { // for each node in graph aka vertex
+            if (isCyclicUtil(graph, visitedSet, stack, node)) { // if cycle found
+                return true; // return true
             }
         }
-        return false;
+        return false; // no cycle found
 
     }
 
-    private static boolean isCyclicUtil(
+    private static boolean isCyclicUtil( // helper method
             Map<String, List<String>> graph,
             Set<String> visited,
             Stack<String> stack, String node) {
 
-        if (stack.contains(node)) {
+        if (stack.contains(node)) { // if node is in current path (node already in stack -> cycle)
             return true;
         }
 
-        if (visited.contains(node)) {
+        if (visited.contains(node)) { // if node is already visited (node already in visitedSet -> no neighbors)
             return false;
         }
 
-        visited.add(node);
+        visited.add(node); // add node to visitedSet
 
         if (graph.containsKey(node)) {
-            stack.push(node);
-            for (String neighbor : graph.get(node)) {
-                if (isCyclicUtil(graph, visited, stack, neighbor)) {
+            stack.push(node); // add node to current path (stack)
+            for (String neighbor : graph.get(node)) { // for each neighbor of node (vertex)
+                if (isCyclicUtil(graph, visited, stack, neighbor)) { // if cycle found
                     return true;
                 }
             }
-            stack.pop();
+            stack.pop(); // remove node from current path (stack) (no neighbors)
         }
 
         return false;
